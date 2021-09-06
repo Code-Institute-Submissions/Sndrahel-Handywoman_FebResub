de3a32b62b36b29193ecbecef0d4e9821c8bfb0e
Sndrahel <sandra.helander@hotmail.com>
2021-09-06 21:26:00 +0000

    fix(home-templates): changed direction for index/home url

--- handywoman/urls.py
+++ handywoman/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
     path("accounts/", include("allauth.urls")),
+    path('', include('home.urls')),
+    path('blog/', include('blog.urls')),
+    
 ]
