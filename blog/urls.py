196f18b95415e25538d39e3064a9479a38c38b96
Sndrahel <sandra.helander@hotmail.com>
2021-09-09 14:57:26 +0000

    Style(blog-core): Add html templates to blog app and add style

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from . import views
+from django.conf.urls import url
 from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    url(r'^edit-post/(?P<pk>\d+)/$', views.edit_post, name='edit_post'),
+    url(r'^delete-post/(?P<pk>\d+)/$', views.delete_post, name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
@@ -65,7 +67,6 @@ class PostDetail(View):
             },
         )
 
-
 class PostLike(View):
 
     def post(self, request, slug, *args, **kwargs):
@@ -76,3 +77,83 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+# Add a new blog post to Blog page
+# Help from Master Code Online
+@login_required
+def add_post(request):
+    """ Add a new blog post """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('blog'))
+
+    form = CommentForm(request.POST or None)
+    template = 'add_post.html'
+
+    try:
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             f'Yahoo! A new blog post has been uploaded \
+                             to the database')
+
+            return redirect(reverse('blog', args=[post.id]))
+
+    except Exception as e:
+        form = CommentForm()
+        messages.warning(request,
+                         'Blog post was not saved. Error: {}'.format(e))
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+# Edit a blog post on Blog page
+# Help from Master Code Online
+@login_required
+def edit_post(request, pk):
+    """ Edit an existing blog post """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('blog'))
+
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             'You have successfully edited this post!')
+            return redirect(reverse('blog'))
+        else:
+            messages.error(request,
+                           'Error: Blog post was not saved.')
+    else:
+        form = CommentForm(instance=post)
+
+    template = 'edit_post.html'
+    context = {
+        'form': form,
+        'post': post,
+        }
+
+    return render(request, template, context)
+
+
+# Delete a post from the blog page
+@login_required
+def delete_post(request, pk):
+    """ Delete a post from the blog """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('blog'))
+
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    messages.success(request, 'Post deleted!')
+    return redirect(reverse('blog'))
