1e6cb106f7f903f37edf17d9e64250ad0ca0bb7a
Sndrahel <sandra.helander@hotmail.com>
2021-09-11 14:49:02 +0000

    Chore(contact-message-core): Add functions for sending email

---
+++ contact/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+
+HANDY_SERVICES = [
+    ("Construction", "Construction"),
+    ("Mechanic", "Mechanic"),
+    ("Logistic", "Logistic"),
+    ("Webb", "Webb"),
+    ("Law", "Law"),
+    ("Other", "Other"),
+]
+
+
+class ContactForm(forms.Form):
+    """ Form for contact page - Send_mail """
+
+    name = forms.CharField(required=True, max_length=90)
+    email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True, max_length=40)
+    handyService = forms.MultipleChoiceField(
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        choices=HANDY_SERVICES,
+        label="Service Selection",
+    )
+    message = forms.CharField(required=True, widget=forms.Textarea(attrs={"rows": 4}))
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from .forms import ContactForm
 
 
 # Create your views here.
 def contact(request):
-    return render(request, 'contact.html', {})
+    contact_qoute_form = ContactForm()
+
+    if request.method == "POST":
+        contact_qoute_form = ContactForm(request.POST)
+        if contact_qoute_form.is_valid():
+            name = request.POST["name"]
+            email = request.POST["email"]
+            subject = request.POST["subject"]
+            handyService = request.POST.getlist("handyService")
+
+            services = ""
+            for i in handyService:
+                if i == handyService[-1]:
+                    services += i
+                else:
+                    services += f"{i}, "
+
+            print(handyService)
+            message = request.POST["message"]
+            send_mail(
+                subject,
+                "From: "
+                + name
+                + "\n\nEmail: "
+                + email
+                + "\n\nType of Service: "
+                + services
+                + "\n\nMessage: "
+                + message,
+                email,
+                ["moscanders@gmail.com"],
+                fail_silently=False,
+            )
+            messages.success(
+                request, "Your message was sent successfully, many Thanks!"
+            )
+            return redirect("contact")
+
+    return render(request, "contact.html", {"form": contact_qoute_form})
