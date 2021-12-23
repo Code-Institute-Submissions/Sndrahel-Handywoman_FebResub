6b99d83a948005f771ef4dc32105d62b75fc3ec5
Sndrahel <sandra.helander@hotmail.com>
2021-12-23 16:06:16 +0000

    Chore(services-core): Added templates, views and urls to services

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.services, name='services'),
+    path('construction/', views.construction, name='construction'),
+    path('logistic/', views.logistic, name='logistic'),
+    path('mechanics/', views.mechanics, name='mechanics'),
+    path('webb/', views.webb, name='webb'),
+    path('law/', views.law, name='law'),
+    path('other/', views.other, name='other'),
 ]
